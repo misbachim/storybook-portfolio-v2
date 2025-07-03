34c369048337c574caf5635cb0de7403e49f4f2f
Misbach <misbachim@gmail.com>
2025-07-03 10:53:20 +0700

    refactor: remove unused metadata and store files to streamline project structure

--- src/app/metadata.ts
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-  title: 'portfolio',
-  description: 'portfolio of Misbach Isron Meitirto',
-  icons: {
-    icon: '/favicon.ico',
-    apple: '/apple-touch-icon.png',
-  },
-  manifest: '/manifest.json',
-} 
